9cf4b9996487adf6a2d504b7499b5a5b53c17b2b
Hemerson Vianna <hemerson.lourenco@gmail.com>
2019-04-02 22:40:12 -0300

    feat: add button with vanilla

--- source/vanilla/button/button.stories.js
+++ source/vanilla/button/button.stories.js
@@ -1,12 +1,13 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 
-storiesOf('Demo', module)
-  .add('heading', () => '<h1>Hello World</h1>')
-  .add('button', () => {
+const stories = storiesOf('Button', module);
+
+stories.add('with text', () => {
     const button = document.createElement('button');
     button.type = 'button';
     button.innerText = 'Hello Button';
     button.addEventListener('click', e => console.log(e));
     return button;
-  });
+  }
+);
