2bc0c316b9f0bc91d20d2b2aea7cc17c1eb8932f
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-01-26 13:26:41 -0300

    fix: update angular config

--- internals/storybook/angular/config.js
+++ internals/storybook/angular/config.js
@@ -1,9 +1,51 @@
-import { configure } from '@storybook/angular';
+import { configure, addParameters, addDecorator } from '@storybook/angular';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withNotes } from '@storybook/addon-notes';
+import { withInfo } from '@storybook/addon-info';
+import { withLinks } from '@storybook/addon-links';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../../../source/angular', true, /.stories.ts$/);
-function loadStories() {
+import '@storybook/addon-console';
+
+addDecorator(withKnobs);
+addDecorator(withNotes);
+addDecorator(withInfo);
+
+addParameters({
+  backgrounds: [
+    { name: 'Default', value: '#f5f5f5', default: true },
+    { name: 'Light', value: '#fff' },
+    { name: 'Dark', value: '#000' },
+  ],
+});
+
+addDecorator(withLinks);
+
+addParameters({
+  options: {
+    name: 'Togakure - Angular',
+    url: 'https://github.com/org-togakure/components',
+    goFullScreen: false,
+    showStoriesPanel: true,
+    showAddonPanel: true,
+    showSearchBox: false,
+    addonPanelInRight: false,
+    sortStoriesByKind: false,
+    hierarchySeparator: /\/|\./, // matches a . or /
+    hierarchyRootSeparator: /\|/, // matches a |
+    sidebarAnimations: true,
+    selectedAddonPanel: undefined,
+    enableShortcuts: false,
+    isToolshown: true
+  }
+});
+
+function importAll(req) {
   req.keys().forEach(filename => req(filename));
+}
+
+function loadStories() {
+  const req = require.context('../../../source/angular', true, /.stories.ts$/);
+  importAll(req);
 }
 
 configure(loadStories, module);
--- internals/storybook/angular/main.js
+++ internals/storybook/angular/main.js
@@ -4,8 +4,9 @@ module.exports = {
   module: {
     rules: [{
       test: /\.css$/,
+      exclude: /node_modules(?!\/@storybook\/addon-info)/,
       use: ["to-string-loader", "style-loader", "css-loader"],
-      include: path.resolve(__dirname, "../../../node_modules/@togakure/styles/lib/css/")
+      include: path.resolve(__dirname, "../../../node_modules/@togakure/styles/lib/css/"),
     }]
   }
 };
